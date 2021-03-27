e15115c7c4b31e2c063db4484f08605bfbb75d7b
SiTingST <33442908+SiTingST@users.noreply.github.com>
2021-03-27 21:24:03 +0800

    Update list command

--- src/main/java/seedu/student/model/Model.java
+++ src/main/java/seedu/student/model/Model.java
@@ -16,6 +16,9 @@ public interface Model {
     /** {@code Predicate} that always evaluate to true */
     Predicate<Student> PREDICATE_SHOW_ALL_STUDENTS = unused -> true;
 
+    /** {@code Predicate} that always evaluate to true */
+    Predicate<Appointment> PREDICATE_SHOW_ALL_APPOINTMENTS = unused -> true;
+
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
      */
